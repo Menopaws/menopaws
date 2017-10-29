a19120ebf41df7f63650f8d4f5a7038fdc19f7dd
claresudbery <clareworking@claresudbery.co.uk>
2017-10-29 12:24:56 +0000

    Add skeleton ClockworkTexting implementation.

---
+++ Menopaws/Menopaws/Texting/ClockworkTexting.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Clockwork;
+
+namespace Menopaws.Texting
+{
+    public class ClockworkTexting : ITexting
+    {
+        public String SendText(String phoneNumber)
+        {
+            return "Success!";
+        }
+    }
+}
