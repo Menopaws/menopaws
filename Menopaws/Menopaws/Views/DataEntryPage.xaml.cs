5e63dbb00368de3cfc73f8edea47e25ef7f7725d
claresudbery <clareworking@claresudbery.co.uk>
2017-10-29 01:04:41 +0000

    Added a new Data Entry page.

---
+++ Menopaws/Menopaws/Views/DataEntryPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Menopaws
+{
+	public partial class DataEntryPage : ContentPage
+	{
+		public DataEntryPage()
+		{
+			InitializeComponent();
+		}
+
+        async void OnDoneButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+    }
+}
