65d5897c5cb3091e9a06588f9893f9a59de41ba4
Luce Carter <luce@lucyscarter.co.uk>
2017-10-29 12:41:25 +0000

    Defualt to current date. Display alert when new date selected - Luce

--- Menopaws/Menopaws/Views/CalendarPage.xaml.cs
+++ Menopaws/Menopaws/Views/CalendarPage.xaml.cs
@@ -10,6 +10,13 @@ namespace Menopaws
 		public CalendarPage()
 		{
 			InitializeComponent();
+			calendar.SelectedDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day);
+			calendar.SelectionChanged += (sender, e) =>
+			{
+				String date = ((Telerik.XamarinForms.Input.RadCalendar)sender).SelectedDate.ToString();
+				date.Substring(0, 10);
+				DisplayAlert("Alert!", date, "OK");
+			};
 		}
 
 		async void OnDoneButtonClicked(object sender, EventArgs e)
