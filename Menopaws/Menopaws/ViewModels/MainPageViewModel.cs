7287cbe831383657e9f7057c9bcefb1762379538
Luce Carter <luce@lucyscarter.co.uk>
2017-10-29 11:45:02 +0000

    Added a blank page for inputting a text and linked the crisis button to it - Luce

--- Menopaws/Menopaws/ViewModels/MainPageViewModel.cs
+++ Menopaws/Menopaws/ViewModels/MainPageViewModel.cs
@@ -10,8 +10,8 @@ namespace Menopaws
 {
 	public class MainPageViewModel : INotifyPropertyChanged
 	{
-		public ICommand AddFlushClicked { get; private set; }
-		public ICommand InputTextClicked { get; private set; }
+
+
 		public INavigation Navigation { get; set; }
 		private ITexting textingService { get; set; }
 
@@ -20,14 +20,8 @@ namespace Menopaws
 		{
 			this.Navigation = navigation;
 			textingService = new StubbedTexting();
-			AddFlushClicked = new Command(async () => await GoToCalendarPage());
-			InputTextClicked = new Command(async () => await GoToTextInputPage());
+
 		}
-
-		public async Task GoToCalendarPage() => await Navigation.PushAsync(new CalendarPage());
-
-		public async Task GoToTextInputPage() => await Navigation.PushAsync(new CalendarPage());
-
 
 		public event PropertyChangedEventHandler PropertyChanged;
 
