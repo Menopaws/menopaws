f70a41b24e72b24e3ea49399a0df2d0d55124494
Luce Carter <luce@lucyscarter.co.uk>
2017-10-29 12:03:35 +0000

    Implement some data validation and stubbed sending of a text - Luce

--- Menopaws/Menopaws/Views/InputTextPage.xaml.cs
+++ Menopaws/Menopaws/Views/InputTextPage.xaml.cs
@@ -8,13 +8,23 @@ namespace Menopaws
 {
 	public partial class InputTextPage : ContentPage
 	{
+		private ITexting textingService { get; set; }
 		public InputTextPage()
 		{
 			InitializeComponent();
+			textingService = new StubbedTexting();
 		}
 
-		async void OnSendClicked(object sender, EventArgs args)
+		void OnSendClicked(object sender, EventArgs args)
 		{
+			if (phoneNumber.Text != null)
+			{
+				DisplayAlert("Alert", textingService.SendText(phoneNumber.Text), "OK");
+			}
+			else
+			{
+				DisplayAlert("Alert", "Please enter a phone number", "OK");
+			}
 
 		}
 	}
