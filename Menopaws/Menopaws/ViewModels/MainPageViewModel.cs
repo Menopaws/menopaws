d5b42323785aafff1f5e86c8b824a6d2a9535ee9
Luce Carter <luce@lucyscarter.co.uk>
2017-10-29 11:30:57 +0000

    Added crisis button - Luce

--- Menopaws/Menopaws/ViewModels/MainPageViewModel.cs
+++ Menopaws/Menopaws/ViewModels/MainPageViewModel.cs
@@ -4,23 +4,29 @@ using System.ComponentModel;
 using Xamarin.Forms;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
+using Menopaws.Texting;
 
 namespace Menopaws
 {
 	public class MainPageViewModel : INotifyPropertyChanged
 	{
 		public ICommand AddFlushClicked { get; private set; }
+		public ICommand InputTextClicked { get; private set; }
 		public INavigation Navigation { get; set; }
+		private ITexting textingService { get; set; }
 
 
 		public MainPageViewModel(INavigation navigation)
 		{
 			this.Navigation = navigation;
+			textingService = new StubbedTexting();
 			AddFlushClicked = new Command(async () => await GoToCalendarPage());
+			InputTextClicked = new Command(async () => await GoToTextInputPage());
 		}
 
 		public async Task GoToCalendarPage() => await Navigation.PushAsync(new CalendarPage());
 
+		public async Task GoToTextInputPage() => await Navigation.PushAsync(new CalendarPage());
 
 
 		public event PropertyChangedEventHandler PropertyChanged;
