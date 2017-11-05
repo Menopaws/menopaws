6a882c7f048ac1782faad2a70edf5106b18a588f
Luce Carter <luce@lucyscarter.co.uk>
2017-11-05 16:49:22 +0000

    Updated so all components on MainPage now use the MainPageViewModel - Luce

--- Menopaws/Menopaws/ViewModels/MainPageViewModel.cs
+++ Menopaws/Menopaws/ViewModels/MainPageViewModel.cs
@@ -3,6 +3,7 @@ using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using Menopaws.Data;
 using Xamarin.Forms;
 
 
@@ -11,16 +12,42 @@ namespace Menopaws
 	public class MainPageViewModel : INotifyPropertyChanged
 	{
 		public INavigation Navigation { get; set; }
+		private IMenopausalDataStore dataStore;
 
-		public ICommand CrisisButtonCommand { get; private set; }
+
+		public ICommand CrisisCommand { get; private set; }
+		public ICommand HotFlushCommand { get; private set; }
+		public Command ChangeMoodCommand { get; private set; }
+
 
 		public MainPageViewModel(INavigation navigation)
 		{
 			this.Navigation = navigation;
-			CrisisButtonCommand = new Command(NavigateSendTextPage);
+			CrisisCommand = new Command(NavigateSendTextPage);
+			HotFlushCommand = new Command(NavigateLogHotFlush);
+			ChangeMoodCommand = new Command(NavigateLogNotMyself);
+			dataStore = new MenopausalDataStub();
 		}
 
 		void NavigateSendTextPage() => Navigation.PushAsync(new InputTextPage());
+
+		void NavigateLogHotFlush()
+		{
+			Navigation.PushAsync(new SchedulePage("Hot Flush"));
+			ListItemSelected = dataStore.GetAllMenopausalEvents();
+		}
+
+		void NavigateLogNotMyself()
+		{
+			Navigation.PushAsync(new SchedulePage("Not feeling myself"));
+			ListItemSelected = dataStore.GetAllMenopausalEvents();
+		}
+
+		public object ListItemSelected
+		{
+			get;
+			private set;
+		}
 
 		public event PropertyChangedEventHandler PropertyChanged;
 
--- Menopaws/Menopaws/Views/MainPage.xaml.cs
+++ Menopaws/Menopaws/Views/MainPage.xaml.cs
@@ -20,36 +20,5 @@ namespace Menopaws
 			dataStore = new MenopausalDataStub();
 			BindingContext = new MainPageViewModel(Navigation);
 		}
-
-		async void OnHotFlushButtonClicked(object sender, EventArgs e)
-		{
-			await Navigation.PushAsync(new SchedulePage("Hot Flush"));
-			listView.ItemsSource = dataStore.GetAllMenopausalEvents();
-		}
-
-		async void OnMoodButtonClicked(object sender, EventArgs e)
-		{
-			await Navigation.PushAsync(new SchedulePage("Change of Mood"));
-		}
-
-		async void OnAddTimeButtonClicked(object sender, EventArgs e)
-		{
-			await Navigation.PushAsync(new TimeEntryPage());
-		}
-
-		async void OnAddDateButtonClicked(object sender, EventArgs e)
-		{
-			await Navigation.PushAsync(new DateEntryPage());
-		}
-
-		async void OnInputTextClicked(object sender, EventArgs args)
-		{
-			await Navigation.PushAsync(new InputTextPage());
-		}
-
-		void OnListItemSelected(object sender, SelectedItemChangedEventArgs e)
-		{
-
-		}
 	}
 }
