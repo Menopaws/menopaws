2428581dc620deef077a6f3ccea642fe7e0aa379
Luce Carter <luce@lucyscarter.co.uk>
2017-12-23 16:28:44 +0000

    Renamed SchedulePageViewModel and moved to own file - Luce

---
+++ Menopaws/Menopaws/ViewModels/SchedulePageViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace UnitTests
+{
+	public class SchedulePageViewModel
+	{
+		public SchedulePageViewModel(IMenopausalDataStore menopausalDataStore)
+		{
+			MenopausalDataStore = menopausalDataStore;
+		}
+
+		public IMenopausalDataStore MenopausalDataStore { get; }
+
+		public void AddHotFlush(MenopausalEvent hotFlush)
+		{
+			MenopausalDataStore.AddEvent(hotFlush);
+		}
+	}
+}
---
+++ UnitTests/ScheduleViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace UnitTests
+{
+	public class ScheduleViewModel
+	{
+		public ScheduleViewModel(IMenopausalDataStore menopausalDataStore)
+		{
+			MenopausalDataStore = menopausalDataStore;
+		}
+
+		public IMenopausalDataStore MenopausalDataStore { get; }
+
+		public void AddHotFlush(MenopausalEvent hotFlush)
+		{
+			MenopausalDataStore.AddEvent(hotFlush);
+		}
+	}
+}
