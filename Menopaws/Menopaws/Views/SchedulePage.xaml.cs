90396735b6d995fcef10a776c841b9ba2a12f334
Clare Sudbery <claresudbery@users.noreply.github.com>
2018-03-29 20:01:54 +0100

    Inject data store into schedule page and add an OnCellTapped handler.

--- Menopaws/Menopaws/Views/SchedulePage.xaml.cs
+++ Menopaws/Menopaws/Views/SchedulePage.xaml.cs
@@ -5,20 +5,33 @@ using Xamarin.Forms;
 using Menopaws.Models;
 using System.Collections.ObjectModel;
 using Syncfusion.SfSchedule.XForms;
+using Menopaws.Data;
 
 namespace Menopaws
 {
 	public partial class SchedulePage : ContentPage
-	{
+    {
+        ScheduleAppointmentCollection scheduleAppointmentCollection;
+        IMenopausalDataStore _dataStore;
+
         public SchedulePage()
 		{
-			InitializeComponent();
+            InitializeComponent();
+            InitialiseSchedule();
 		}
 
-		public SchedulePage(string callingPageName)
+        public SchedulePage(string callingPageName, IMenopausalDataStore dataStore)
 		{
 			InitializeComponent();
+            _dataStore = dataStore;
+            InitialiseSchedule();
 		}
+
+        private void InitialiseSchedule() 
+        {
+            schedule.CellDoubleTapped += OnCellTapped;
+            scheduleAppointmentCollection = new ScheduleAppointmentCollection();
+        }
 
 		async void OnDoneButtonClicked(object sender, EventArgs e)
 		{
@@ -28,6 +41,11 @@ namespace Menopaws
 		async void OnInputTextClicked(object sender, EventArgs args)
 		{
 			await Navigation.PushAsync(new InputTextPage());
-		}
+        }
+
+        void OnCellTapped(object sender, CellTappedEventArgs e)
+        {
+            DisplayAlert("Tapped!", sender.ToString(), "OK");
+        }
 	}
 }
